7d21e4b6a6edc46d08f8df4f1db1da2404035353
your_username <your_email@example.com>
2015-12-29 14:32:16 -0500

    Update data documentation for NWEA Norms Status 2015

--- R/norms_status_2015.R
+++ R/norms_status_2015.R
@@ -1,18 +1,26 @@
 #' NWEA Norms data 2015 (status)
 #'
-#' On-time data for all flights that departed NYC (i.e. JFK, LGA or EWR) in
-#' 2013.
+#' Data from the RIT Scale Norms Study 2015
 #'
 #' @source NWEA, Northwest Evaluation Association,
 #'  \url{https://legacysupport.nwea.org/support/article/norms-study-resources}
 #' @format Data frame with columns
 #' \describe{
-#' \item{season}{Term Possible Values: 4 = Beginning of year (Fall)
-#' 1 = Middle of year (Winter) 2 = End of year (Spring)
+#' \item{season}{Term. Possible Values: 
+#' 4 = Beginning of year (Fall),
+#' 1 = Middle of year (Winter),
+#' 2 = End of year (Spring),
 #' 3 = Norms not available for this season (Summer)"}
-#' \item{grade}{Grade}
-#' \item{subject}{Subject}
-#' \item{student_pctile}{Student percentile}
+#' \item{grade}{Focal grade (the grade regarded as the present grade of focus for the purposes of norms look-up)
+#' Grade 13 refers to kindergarten}
+#' \item{subject}{Measurement Scale.	Possible Values: 
+#' 1 = Mathematics,
+#' 2 = Reading,
+#' 3 = Language Usage
+#' 4 = General science
+#' As of 2015, Science Concepts and Processes is no longer available}
+#' \item{student_pctile}{Percentile associated with student RIT score for subject, grade, and season
+#' Range = 1-99%}
 #' \item{rit}{RIT score}
 #' }
 "norms_status_2015"
