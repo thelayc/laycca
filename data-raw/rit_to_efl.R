157663f800928f2e746f2ccc5fb8b5cddd50fd6d
your_username <your_email@example.com>
2016-01-14 14:33:33 -0500

    update equivalency table

--- data-raw/rit_to_efl.R
+++ data-raw/rit_to_efl.R
@@ -13,6 +13,15 @@ rit_to_efl <- rit_to_efl %>%
 # Remove duplicate values of RIT
 rit_to_efl <- rit_to_efl[!duplicated(rit_to_efl$rit, fromLast = TRUE), ]
 
+# Add rows for low values of RIT that are not in the original dataset
+rit <- 150:175
+tabe_reading <- rep(NA, 26)
+lexile <- rep(NA, 26)
+nrs_efl <- rep(1, 26)
+temp <- data.frame(rit, tabe_reading, lexile, nrs_efl)
+rit_to_efl <- rbind(temp, rit_to_efl)
+
+
 ## TO DO:
 # Deal with duplicate rit value of 188
 
