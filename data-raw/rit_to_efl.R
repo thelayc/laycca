86bb0ee1ebec3deca8a6370132dd5a0644ca6951
your_username <your_email@example.com>
2016-01-14 10:29:51 -0500

    update equivalency table

--- data-raw/rit_to_efl.R
+++ data-raw/rit_to_efl.R
@@ -7,8 +7,11 @@ library(dplyr)
 
 rit_to_efl <- rit_to_efl %>%
   filter(!is.na(rit)) %>%
-  select(rit, tabe_reading:nrs_efl)
+  select(rit, tabe_reading:nrs_efl) %>%
+  arrange(rit, tabe_reading)
 
+# Remove duplicate values of RIT
+rit_to_efl <- rit_to_efl[!duplicated(rit_to_efl$rit, fromLast = TRUE), ]
 
 ## TO DO:
 # Deal with duplicate rit value of 188
